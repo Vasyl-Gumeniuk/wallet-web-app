301847297b35111d0c7c1770dd29168ec3b91dac
Oleksandr Sainchuk <sainchyksasha2015999@gmail.com>
2022-11-20 00:10:54 +0200

    Adds EmailInput component

--- src/pages/LoginPage/LoginForm/LoginForm.jsx
+++ src/pages/LoginPage/LoginForm/LoginForm.jsx
@@ -1,5 +1,35 @@
+import { Form, Formik } from 'formik';
+import EmailInput from '../../../components/EmailInput';
+
 const LoginForm = () => {
-  return <h1>Login Form</h1>;
+  return (
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      // validationSchema={SignupSchema}
+      // onSubmit={async (values, actions) => {
+      //   const validValues = {
+      //     ...values,
+      //     position: +values.position,
+      //     file,
+      //   };
+      //   await handleSubmit(validValues);
+      //   if (postError !== '') {
+      //     actions.setSubmitting(false);
+      //   }
+      // }}
+    >
+      {({ handleChange, isSubmitting, values, errors, touched }) => {
+        return (
+          <Form>
+            <EmailInput />
+          </Form>
+        );
+      }}
+    </Formik>
+  );
 };
 
 export default LoginForm;
