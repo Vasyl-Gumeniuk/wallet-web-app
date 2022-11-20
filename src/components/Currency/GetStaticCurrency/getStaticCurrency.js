e6a88589df353fbacacbd47fe520c1f6894e48be
Vasyl-Gumeniuk <vasyl.gumeniuk@gmail.com>
2022-11-20 23:31:29 +0200

    add static currency

---
+++ src/components/Currency/GetStaticCurrency/getStaticCurrency.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { reqOptionsEur, reqOptionsUsd, reqOptionsGbp } from './option';
+
+const baseURL = 'https://currency-exchange.p.rapidapi.com/exchange';
+
+export const useGetStaticCurrency = () => {
+  const [currency, SetCurrency] = useState({ eur: 0, usd: 0, gbp: 0 });
+
+  const getUSD = () => axios.get(baseURL, reqOptionsUsd);
+  const getEUR = () => axios.get(baseURL, reqOptionsEur);
+  const getGBP = () => axios.get(baseURL, reqOptionsGbp);
+
+  useEffect(
+    () => () =>
+      axios
+        .all([getEUR(), getUSD(), getGBP()])
+        .then(r =>
+          SetCurrency({
+            eur: Math.trunc(r[0].data * 100) / 100,
+            usd: Math.trunc(r[1].data * 100) / 100,
+            gbp: Math.trunc(r[2].data * 100) / 100,
+          }),
+        )
+        .catch(e => console.log(e)),
+
+    [SetCurrency],
+  );
+
+  return currency;
+};
