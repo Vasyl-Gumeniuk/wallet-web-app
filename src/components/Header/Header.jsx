d6976db723d3326d586242d233c159972df10bd7
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-11-20 21:50:39 +0200

    loader

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
+import css from './Header.module.css';
+import sprite from '../../images/icons/sprite-all-icons.svg'
 
-export default function Hello() {
+export default function Header() {
     
     return (
-        <div>
-          <h1>Welcome to Wallet Web Application</h1>  
+        <div className={css.header}>
+          <div className={css.header_block}>
+            <div className={css.logo}>
+              <svg width="40" height="40" aria-label="wallet">
+                <use href={`${sprite}#icon-wallet`}></use>
+              </svg>
+              <p>Wallet</p>
+            </div>
+            <div className={css.user_block}>
+              <p className={css.name}>Name</p>
+              <svg className={css.icon_exit} width="18" height="18" aria-label="wallet">
+                <use href={`${sprite}#icon-exit`}></use>
+              </svg>
+              <p className={css.exit}>Exit</p>
+            </div>
+          </div>
         </div>
     );
 };
