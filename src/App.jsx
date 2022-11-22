f356b2be8897f5d6a6e8cc4a20e7123b6fe29627
Kristina Tuzova <ktuzova1901@gmail.com>
2022-11-22 04:56:18 +0200

    deleted

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { GlobalStyle } from './GlobalStyle';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import { Routes, Route } from 'react-router-dom';
-import Currency from './components/Currency/Currency'; 
 export const App = () => {
   return (
     <>
