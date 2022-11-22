b83f36af0067899f5856acc2585c62fb2ba5714d
Kristina Tuzova <ktuzova1901@gmail.com>
2022-11-22 04:52:54 +0200

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { GlobalStyle } from './GlobalStyle';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import { Routes, Route } from 'react-router-dom';
+import Currency from './components/Currency/Currency'; 
 export const App = () => {
   return (
     <>
