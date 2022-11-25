eb6a0ebcf1d6609a4033765a08568cea0ce502a3
Kristina Tuzova <ktuzova1901@gmail.com>
2022-11-25 08:54:55 +0200

    currency cod and styles

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -6,11 +6,43 @@ import {CellLast} from './Currency.styled'
 import {LineNotFirst} from './Currency.styled'
 import {Foot} from './Currency.styled'
 import {Line} from './Currency.styled'
-import {useGetStaticCurrency} from './GetStaticCurrency/getStaticCurrency'
+import Loader from "../Loader/Loader";
+import {useEffect, useState } from 'react';
 export default function Currency() {
-    const {eur, usd, gbp} = useGetStaticCurrency();
-    return (
-        <Wrapper>
+    const today = Date.now()
+    const [currency, SetCurrency] = useState(() => showCurrency());
+     async function showCurrency() {
+        let response = await fetch(
+          'https://api.monobank.ua/bank/currency'
+        );
+        if (response.status === 200) {
+          let json = await response.json();
+          localStorage.setItem('time', JSON.stringify(today));
+          SetCurrency(filterCurrency(json))
+          return filterCurrency(json);
+        }
+        throw new Error(response.status);
+      }
+      useEffect(()=>{
+        let time = JSON.parse(localStorage.getItem('time'));
+        if(time + 3600000 < today) {
+          showCurrency()
+        }
+      })
+      function filterCurrency(array) {
+        let currencyUSD = array.filter(item => item.currencyCodeA === 840)
+        let currencyEUR = array.filter(item => item.currencyCodeA === 978)
+        let currencyGBP = array.filter(item => item.currencyCodeA === 826)
+        var currencyArray = currencyUSD.concat(currencyEUR, currencyGBP);
+        return currencyArray
+      }
+      let dataforRendering
+      if(currency[0] === undefined) {
+        dataforRendering = <Loader/>
+      }
+      else {
+        dataforRendering = (
+         <Wrapper>
         <Header>
         <Line>
         <Cell>Currency</Cell>
@@ -21,24 +53,26 @@ export default function Currency() {
         <Body>
         <Line>
         <Cell>USD</Cell>
-        <Cell>{usd}</Cell>
-        <CellLast>{usd}</CellLast>
+        <Cell>{currency[0].rateBuy}</Cell>
+        <CellLast>{currency[0].rateSell.toFixed(2)}</CellLast>
         </Line>
         <Line>
         <LineNotFirst>EUR</LineNotFirst>
-        <LineNotFirst>{eur}</LineNotFirst>
-        <LineNotFirst>{eur}</LineNotFirst>
+        <LineNotFirst>{currency[1].rateBuy}</LineNotFirst>
+        <LineNotFirst>{currency[1].rateSell.toFixed(2)}</LineNotFirst>
         </Line>
         <Line>
         <LineNotFirst>GBP</LineNotFirst>
-        <LineNotFirst>{gbp}</LineNotFirst>
-        <LineNotFirst>{gbp}</LineNotFirst>
+        <LineNotFirst>{currency[3].rateCross.toFixed(2)}</LineNotFirst>
+        <LineNotFirst>{currency[3].rateCross.toFixed(2)}</LineNotFirst>
         </Line>
         </Body>
         <Foot/>
     
      </Wrapper>
-    
-     
-    );
+        )
+      }
+    return (
+    dataforRendering
+ );
 };
