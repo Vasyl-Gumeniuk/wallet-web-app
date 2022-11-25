a1f76c7fc1c3396d507d5c8c160e1cb80929467d
Kristina Tuzova <ktuzova1901@gmail.com>
2022-11-25 10:00:49 +0200

    code and style

--- src/components/Currency/Currency.styled.js
+++ src/components/Currency/Currency.styled.js
@@ -1,123 +1,78 @@
-import styled from 'styled-components';
-import vector from '../../images/currency/vector.svg'
-export const Wrapper = styled.table`
-  background-color: #4A56E2;
-   color:#FFFFFF;
-   border-collapse: separate;
-   border-spacing: 0;
-   font-size: 16px; 
-   border-radius: 30px;
-   overflow: hidden;
-   font-family: 'Circe';
-   @media screen and (min-width: 1024px) {
-    background-image: url(${vector});
-    background-repeat: no-repeat;
-    background-position: left bottom;
-    background-size: contain;
-  }
-  @media screen and (min-width: 2560px) {
-    background-image: url(${vector});
-    background-repeat: no-repeat;
-    background-position: left bottom;
-    background-size: contain;
-   }
-    `;
-
-  export const Header = styled.thead`
-  font-size: 18px; 
-  background-color: rgba(255, 255, 255, 0.2);
-  font-weight: 700;
-  font-family: 'Circe';
-  line-height: 27px;
-  
-  `;
-  export const Line = styled.tr`
-    border: none
-  `;
-  export const Body = styled.tbody`
-  font-size: 16px;
-  background-color: #4A56E2;
-  font-weight: 400;
-  line-height: 24px;
-  background-image: url(${vector});
-  background-repeat: no-repeat;
-  background-position: left bottom;
-  background-size: contain;
-  @media screen and (min-width: 1024px) {
-    background-image: none;
-  }
-  @media screen and (min-width: 2560px) {
-    background-image: none;     
-   }
-  `;
-   export const Cell = styled.td`
-   padding-top: 12px;
-   padding-left: 20px;
-   padding-bottom: 11px; 
-   font-family: 'Circe';
-   font-size: 16px;
-   line-height: 24px;
-   border:none;
-
-   
-  @media screen and (min-width: 1024px) {
-    padding-left: 58px;
-  }
-
-  @media screen and (min-width: 2560px) {
-    padding-left: 58px;
+import { Wrapper } from "./Currency.styled";
+import {Header} from './Currency.styled'
+import {Body} from './Currency.styled'
+import {Cell} from './Currency.styled'
+import {CellLast} from './Currency.styled'
+import {LineNotFirst} from './Currency.styled'
+import {Foot} from './Currency.styled'
+import {Line} from './Currency.styled'
+import Loader from "../Loader/Loader";
+import {useEffect, useState } from 'react';
+export default function Currency() {
+    const today = Date.now()
+    const [currency, SetCurrency] = useState(() => showCurrency());
+     async function showCurrency() {
+        let response = await fetch(
+          'https://api.monobank.ua/bank/currency'
+        );
+        if (response.status === 200) {
+          let json = await response.json();
+          localStorage.setItem('time', JSON.stringify(today));
+          SetCurrency(filterCurrency(json))
+          return filterCurrency(json);
+        }
+        throw new Error(response.status);
+      }
+      useEffect(()=>{
+        let time = JSON.parse(localStorage.getItem('time'));
+        if(time + 3600000 < today) {
+          showCurrency()
+        }
+      })
+      function filterCurrency(array) {
+        let currencyUSD = array.filter(item => item.currencyCodeA === 840)
+        let currencyEUR = array.filter(item => item.currencyCodeA === 978)
+        let currencyGBP = array.filter(item => item.currencyCodeA === 826)
+        var currencyArray = currencyUSD.concat(currencyEUR, currencyGBP);
+        return currencyArray
+      }
+      let dataforRendering
+      if(currency[0] === undefined) {
+        dataforRendering = <Loader/>
+      }
+      else {
+        dataforRendering = (
+         <Wrapper>
+        <Header>
+        <Line>
+        <Cell>Currency</Cell>
+        <Cell>Purchase</Cell>
+        <CellLast>Sale</CellLast>
+        </Line>
+        </Header>
+        <Body>
+        <Line>
+        <Cell>USD</Cell>
+        <Cell>{currency[0].rateBuy}</Cell>
+        <CellLast>{currency[0].rateSell.toFixed(2)}</CellLast>
+        </Line>
+        <Line>
+        <LineNotFirst>EUR</LineNotFirst>
+        <LineNotFirst>{currency[1].rateBuy}</LineNotFirst>
+        <LineNotFirst>{currency[1].rateSell.toFixed(2)}</LineNotFirst>
+        </Line>
+        <Line>
+        <LineNotFirst>GBP</LineNotFirst>
+        <LineNotFirst>{currency[3].rateCross.toFixed(2)}</LineNotFirst>
+        <LineNotFirst>{currency[3].rateCross.toFixed(2)}</LineNotFirst>
+        </Line>
+        </Body>
+        <Foot/>
     
-  }
-   `;
-   export const CellLast = styled.td`
-   padding-left: 20px;
-   padding-top: 12px;
-   padding-right: 20px;
-   padding-bottom: 11px; 
-   font-family: 'Circe';
-   font-size: 16px;
-   line-height: 24px;
-
-   @media screen and (min-width: 1024px) {
-    padding-left: 58px;
-    padding-right: 58px;
-  }
-
-  @media screen and (min-width: 2560px) {
-    padding-left: 58px;
-    padding-right: 58px;
-    
-  }
-
-   `;
-   export const LineNotFirst = styled.td`
-   padding-left: 20px;
-   padding-right: 20px;
-   padding-bottom: 12px; 
-   font-family: 'Circe';
-   font-size: 16px;
-   line-height: 24px;
-   @media screen and (min-width: 1024px) {
-    padding-left: 58px;
-  }
-
-  @media screen and (min-width: 2560px) {
-    padding-left: 58px;
-    
-  }
-  `;
-  export const Foot = styled.tfoot`
-   dispay: none;  
-   background-image: url(${vector});
-   background-repeat: no-repeat;
-   background-position: left bottom;
-   background-size: contain;
- @media screen and (min-width: 1024px) {
-  dispay: block;  
-  height: 134px;
-  }
- @media screen and (min-width: 2560px) {
-  dispay: block;  
-   height: 134px;
- }
-  `;
+     </Wrapper>
+        )
+      }
+    return (
+    dataforRendering
+ );
+};
