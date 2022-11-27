a9d9af4d4b9e415d665830cd7dccad948c81b1d4
Kristina Tuzova <ktuzova1901@gmail.com>
2022-11-28 00:30:09 +0200

    components

---
+++ src/components/Inputs/Currency/Currency.jsx
@@ -0,0 +1,92 @@
+import { Wrapper } from "./Currency.styled";
+import {Header} from './Currency.styled'
+import {Body} from './Currency.styled'
+import {Cell} from './Currency.styled'
+import {CellLast} from './Currency.styled'
+import {Foot} from './Currency.styled'
+import {Line} from './Currency.styled'
+import Loader from "../Loader/Loader";
+import {CellMiddle} from './Currency.styled'
+import {useEffect, useState} from 'react';
+import {CellBody} from './Currency.styled'
+import {CellMiddleBody} from './Currency.styled'
+import {CellLastBody} from './Currency.styled'
+import {CellMiddleBodyLast} from './Currency.styled'
+export default function Currency() {
+     const [currency, SetCurrency] = useState([]);
+     useEffect(() =>{
+       const today = Date.now()
+        let time = localStorage.getItem('time');
+        let currencyArray  = async () => {
+        let response = await fetch(
+            'https://api.monobank.ua/bank/currency'
+          );
+      if (response.status === 200) {
+            let json = await response.json();
+            localStorage.setItem('time', JSON.stringify(today));
+            SetCurrency(filterCurrency(json))
+            return filterCurrency(json)
+        }
+       }
+      if(localStorage.getItem('currency')=== null) {
+        currencyArray().then(data=>localStorage.setItem('currency', JSON.stringify(data)))
+        
+      }
+      if(localStorage.getItem('currency')!== null &&time + 3600000 < today) {
+        currencyArray()
+        }
+      if(localStorage.getItem('currency')!== null&& time + 3600000 >= today) {
+        let currency = JSON.parse(localStorage.getItem('currency'));
+        SetCurrency(currency)
+        }
+        return () => {
+
+        }
+     },[])
+      function filterCurrency(array) {
+        let currencyUSD = array.filter(item => item.currencyCodeA === 840)
+        let currencyEUR = array.filter(item => item.currencyCodeA === 978)
+        let currencyGBP = array.filter(item => item.currencyCodeA === 826)
+        var currencyArray = currencyUSD.concat(currencyEUR, currencyGBP);
+        return currencyArray
+      }
+      let dataforRendering
+      if(currency[0] === undefined) {
+        dataforRendering = <Loader/>
+      }
+      else {
+        dataforRendering = (
+         <Wrapper>
+        <Header>
+        <Line>
+        <Cell>Currency</Cell>
+        <CellMiddle>Purchase</CellMiddle>
+        <CellLast>Sale</CellLast>
+        </Line>
+        </Header>
+        <Body>
+        <Line>
+        <CellBody>USD</CellBody>
+        <CellMiddleBody>{currency[0].rateBuy}</CellMiddleBody>
+        <CellBody>{currency[0].rateSell.toFixed(2)}</CellBody>
+        </Line>
+        <Line>
+        <CellBody>EUR</CellBody>
+        <CellMiddleBody>{currency[1].rateBuy}</CellMiddleBody>
+        <CellBody>{currency[1].rateSell.toFixed(2)}</CellBody>
+        </Line>
+        <Line>
+        <Cell>GBP</Cell>
+        <CellMiddleBodyLast>{currency[3].rateCross.toFixed(2)}</CellMiddleBodyLast>
+        <CellLastBody>{currency[3].rateCross.toFixed(2)}</CellLastBody>
+        </Line>
+        </Body>
+        <Foot/>
+    
+     </Wrapper>
+        )
+      }
+    return (
+    dataforRendering
+ );
+};
---
+++ src/components/Inputs/Currency/Currency.styled.js
@@ -0,0 +1,237 @@
+import styled from 'styled-components';
+import vector from '../../images/currency/vector.svg'
+export const Wrapper = styled.table`
+  background-color: #4A56E2;
+   color:#FFFFFF;
+   border-collapse: separate;
+   border-spacing: 0;
+   font-size: 16px; 
+   border-radius: 30px;
+   overflow: hidden;
+   font-family: 'Circe';
+   @media screen and (min-width: 1024px) {
+    background-image: url(${vector});
+    background-repeat: no-repeat;
+    background-position: left bottom;
+    background-size: contain;
+  }
+  @media screen and (min-width: 2560px) {
+    background-image: url(${vector});
+    background-repeat: no-repeat;
+    background-position: left bottom;
+    background-size: contain;
+   }
+    `;
+
+  export const Header = styled.thead`
+  font-size: 18px; 
+  background-color: rgba(255, 255, 255, 0.2);
+  font-weight: 700;
+  font-family: 'Circe';
+  line-height: 27px;
+  
+  `;
+  export const Line = styled.tr`
+    border: none
+  `;
+  export const Body = styled.tbody`
+  font-size: 16px;
+  background-color: #4A56E2;
+  font-weight: 400;
+  line-height: 24px;
+  background-image: url(${vector});
+  background-repeat: no-repeat;
+  background-position: left bottom;
+  background-size: contain;
+  @media screen and (min-width: 1024px) {
+    background-image: none;
+  }
+  @media screen and (min-width: 2560px) {
+    background-image: none;     
+   }
+  `;
+   export const Cell = styled.td`
+   padding-top: 12px;
+   padding-left: 20px;
+   padding-bottom: 12px; 
+   font-family: 'Circe';
+   font-size: 16px;
+   line-height: 24px;
+   border:none;
+   font-weight: 400;
+
+   
+  @media screen and (min-width: 1024px) {
+    padding-left: 44px;
+  }
+
+  @media screen and (min-width: 2560px) {
+    padding-left: 44px;
+    
+  }
+  @media screen and (min-width: 768px) {
+    padding-bottom:16px;
+   }
+   `;
+   export const CellLast = styled.td`
+   padding-left: 20px;
+   padding-top: 12px;
+   padding-right: 20px;
+   padding-bottom: 12px; 
+   font-family: 'Circe';
+   font-size: 16px;
+   line-height: 24px;
+   font-weight: 400;
+   text-align: center;
+
+   @media screen and (min-width: 1024px) {
+    padding-left: 26px;
+    padding-right: 64px;
+  }
+
+  @media screen and (min-width: 2560px) {
+    padding-left: 26px;
+    padding-right: 64px;
+    
+  }
+  @media screen and (min-width: 768px) {
+   padding-bottom:16px;
+  }
+  `;
+  export const CellLastBody = styled.td`
+  padding-left: 20px;
+  padding-top: 12px;
+  padding-right: 20px;
+  padding-bottom: 12px; 
+  font-family: 'Circe';
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+
+  @media screen and (min-width: 1024px) {
+   padding-left: 44px;
+   padding-right: 64px;
+   padding-top: 22px;
+ }
+
+ @media screen and (min-width: 2560px) {
+   padding-left: 44px;
+   padding-right: 64px;
+   padding-top: 22px;
+   
+ }
+ @media screen and (min-width: 768px) {
+  padding-bottom:16px;
+ }
+ `;
+   export const Foot = styled.tfoot`
+   dispay: none;  
+  @media screen and (min-width: 1024px) {
+  dispay: block;  
+  height: 134px;
+  }
+ @media screen and (min-width: 2560px) {
+  dispay: block;  
+   height: 134px;
+ }
+  `;
+  export const CellMiddle = styled.td`
+  padding-top: 12px;
+  padding-left: 33px;
+  padding-right: 33px;
+  padding-bottom: 12px; 
+  font-family: 'Circe';
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+  @media screen and (min-width: 768px) {
+    padding-left: 61px;
+    padding-right: 61px;
+    padding-bottom:16px;
+  }
+  @media screen and (min-width: 1024px) {
+    padding-left: 40px;
+    padding-right: 40px;
+  }
+
+  @media screen and (min-width: 2560px) {
+    padding-left: 40px;
+    padding-right: 40px;
+    
+  }
+    `;
+   export const CellBody = styled.td`
+   padding-top: 12px;
+   padding-left: 20px;
+   font-family: 'Circe';
+   font-size: 16px;
+   line-height: 24px;
+   border:none;
+   font-weight: 400;
+   @media screen and (min-width: 1024px) {
+    padding-left: 44px;
+    padding-right: 44px;
+    padding-top: 22px;
+  }
+
+Kristi, [26.11.2022 22:41]
+@media screen and (min-width: 2560px) {
+    padding-left: 44px;
+    padding-right: 44px;
+    padding-top: 22px;
+    
+  }
+ `;
+ export const CellMiddleBody = styled.td`
+  padding-top: 12px;
+  padding-left: 33px;
+  padding-right: 33px;
+  font-family: 'Circe';
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+  text-align: center;
+  @media screen and (min-width: 768px) {
+    padding-left: 61px;
+    padding-right: 61px;
+  }
+  @media screen and (min-width: 1024px) {
+    padding-left: 29px;
+    padding-right: 29px;
+    padding-top: 22px;
+  }
+
+  @media screen and (min-width: 2560px) {
+    padding-left: 29px;
+    padding-right: 29px;
+    padding-top: 22px;
+    
+  }
+    `;
+    export const CellMiddleBodyLast = styled.td`
+    padding-left: 20px;
+    padding-top: 12px;
+    padding-right: 20px;
+    padding-bottom: 12px; 
+    font-family: 'Circe';
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+    text-align:center;
+    @media screen and (min-width: 1024px) {
+     padding-left: 29px;
+     padding-right: 29px;
+     padding-top: 22px;
+   }
+  
+   @media screen and (min-width: 2560px) {
+     padding-left: 29px;
+     padding-right: 29px;
+     padding-top: 22px;
+     
+   }
+   @media screen and (min-width: 768px) {
+    padding-bottom:16px;
+    
+   }
+    `;
