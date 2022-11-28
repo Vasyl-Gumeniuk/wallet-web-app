9f026957f633525d6f3f157e73ae0e7145c81060
Oleksandr Sainchuk <sainchyksasha2015999@gmail.com>
2022-11-28 12:21:07 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,6 +4,12 @@ import Logo from '../../components/Logo';
 import LoginForm from './LoginForm';
 
 const LoginPage = () => {
+<<<<<<< Updated upstream
+=======
+  const isNotMobile = useMedia('(min-width: 768px)');
+  const isDesktop = useMedia('(min-width: 1280px)');
+
+>>>>>>> Stashed changes
   return (
     <Box pt={107} pb={107}>
       <ContainerBlock>
