8ad608ed3446c15368afd6c36bfb7507a2fbc89a
Oleksandr Sainchuk <sainchyksasha2015999@gmail.com>
2022-11-28 12:49:05 +0200

    Adds frame to laptop

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,22 +1,32 @@
-import { Box } from '../../Box';
-import { ContainerBlock } from '../../components/Container/Container.styled';
+import { useMedia } from 'react-use';
+import { AuthContainer } from '../../components/Container/Container.styled';
 import Logo from '../../components/Logo';
 import LoginForm from './LoginForm';
+import { AuthSection, FramePNG } from './LoginPage.styled';
+import frame1x from '../../images/loginPage/tab/Frame-1x.png';
+import frame2x from '../../images/loginPage/tab/Frame-2x.png';
 
 const LoginPage = () => {
-<<<<<<< Updated upstream
-=======
   const isNotMobile = useMedia('(min-width: 768px)');
   const isDesktop = useMedia('(min-width: 1280px)');
-
->>>>>>> Stashed changes
   return (
-    <Box pt={107} pb={107}>
-      <ContainerBlock>
+    <AuthSection>
+      <AuthContainer>
         <Logo type="login" />
         <LoginForm />
-      </ContainerBlock>
-    </Box>
+      </AuthContainer>
+      {isNotMobile && (
+        <FramePNG>
+          <img
+            srcSet={`${frame2x} 1280w, ${frame1x} 768w`}
+            src={frame1x}
+            alt="frame"
+            width={isDesktop ? 435 : 260}
+            height={isDesktop ? 420 : 250}
+          />
+        </FramePNG>
+      )}
+    </AuthSection>
   );
 };
 
