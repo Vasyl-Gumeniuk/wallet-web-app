6ffe0dcf8c052fcae9adb06315c638c7bf86e8ca
Andrey Pakhomov <pahomeathome@gmail.com>
2022-11-30 14:27:08 +0200

    ModalLogout actions added

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -1,8 +1,11 @@
+import { useDispatch } from 'react-redux';
 import { Formik } from 'formik';
 import { Form } from 'formik';
 import { Box } from '../../Box';
 import { ContainerBlock } from '../Container/Container.styled';
 import { ButtonElement } from '../../pages/LoginPage/LoginForm/LoginForm.styled';
+import { logOutUser } from '../../redux/auth/authSlice';
+import { useLogOutMutation } from '../../redux/auth/authApi';
 import {
   Title,
   CenteredDiv,
@@ -10,7 +13,21 @@ import {
   CancelButton,
 } from './ModalLogout.styled';
 
-export const ModalLogout = () => {
+export const ModalLogout = ({ closeModal }) => {
+  const [logout, { isSuccess }] = useLogOutMutation();
+  const dispatch = useDispatch();
+  const handleLogOut = async () => {
+    try {
+      await logout();
+      if (isSuccess) {
+        await dispatch(logOutUser());
+        closeModal();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <CenteredDiv>
       <Box pt={32} pb={32}>
@@ -22,10 +39,12 @@ export const ModalLogout = () => {
                 <Form>
                   <ul>
                     <ButtonElement>
-                      <LogoutButton type="submit">Log out</LogoutButton>
+                      <LogoutButton onClick={handleLogOut}>
+                        Log out
+                      </LogoutButton>
                     </ButtonElement>
                     <ButtonElement>
-                      <CancelButton>Cancel</CancelButton>
+                      <CancelButton onClick={closeModal}>Cancel</CancelButton>
                     </ButtonElement>
                   </ul>
                 </Form>
